f8b5ee668d9becfed2a87f662d022bf29abbeab6
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2016-04-30 18:10:40 -0400

    WP Admin Form
    Need to still validate input in update.

--- fake-stocks.php
+++ fake-stocks.php
@@ -38,6 +38,46 @@ class AI_Fake_Stocks extends WP_Widget {
 	 */
 	public function form( $instance ) {
 		// outputs the options form on admin
+        if ( isset( $instance['title'] ) ) {
+            $title = $instance['title'];   
+        } else {
+            $title = 'Stock Quotes';
+        }
+        if ( isset( $instance['api_key'] ) ) {
+            $api_key = $instance['api_key'];   
+        } else {
+            $api_key = '';
+        }
+        if ( isset( $instance['num'] ) ) {
+            $num = $instance['num'];   
+        } else {
+            $num = 3;
+        }
+        if ( isset( $instance['symbols'] ) ) {
+            $symbols = $instance['symbols'];   
+        } else {
+            $symbols = 'AAPL, MSFT, INTC, GILD, AAL';
+        }
+        
+         //Widget Title Field
+         echo '<p><label for="' . $this ->get_field_id('title') . '">Title:</label>';
+         echo '<input class="widefat" type="text" value="'. esc_attr( $title ) . '" id="' . $this->get_field_id( 'title' ) . 
+                '" name="' . $this->get_field_name( 'title' ) . '"></p>';
+          
+         //API Key Field      
+         echo '<p><label for="' . $this ->get_field_id('api_key') . '">Your API Key:</label>';
+         echo '<input class="widefat" type="text" value="'. esc_attr( $api_key ) . '" id="' . $this->get_field_id( 'api_key' ) . 
+                '" name="' . $this->get_field_name( 'api_key' ) . '"></p>';
+                
+         //Number of Stocks Field      
+         echo '<p><label for="' . $this ->get_field_id('num') . '">Number of stocks quotes to show:</label>';
+         echo '<input class="tiny-text" type="number" step="1" min="1" value="'. esc_attr( $num ) . '" size="3" id="' .
+                $this->get_field_id( 'num' ) . '" name="' . $this->get_field_name( 'num' ) . '"></p>';
+                
+         //Stock Symbols Field      
+         echo '<p><label for="' . $this ->get_field_id('symbols') . '">NASDAQ ticker symbols (separated by commas):</label>';
+         echo '<input class="widefat" type="text" value="'. esc_attr( $symbols ) . '" id="' . $this->get_field_id( 'symbols' ) . 
+                '" name="' . $this->get_field_name( 'symbols' ) . '"></p>';
 	}
 
 	/**
@@ -48,6 +88,7 @@ class AI_Fake_Stocks extends WP_Widget {
 	 */
 	public function update( $new_instance, $old_instance ) {
 		// processes widget options to be saved
+        $instance['title'] = strip_tags( $new_instance['title'] );
 	}
 }
 
